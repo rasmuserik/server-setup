16d382da2777166d4c41e19bc7d9454a57c452e1
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-03-20 13:30:16 +0100

    remove sudo

--- dump.sh
+++ dump.sh
@@ -1,7 +1,7 @@
-sudo rm -f dump.tar.bz2
+rm -f dump.tar.bz2
 for x in tinkuy-nodebb tinkuy-redis
 do
   echo dumping $x
-  sudo tar rpPf dump.tar /var/lib/docker/volumes/whale_$x
+  tar rpPf dump.tar /var/lib/docker/volumes/whale_$x
 done
 bzip2 -9 dump.tar
