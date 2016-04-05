eed417b1c2c753cd8e561310fc57d3388baeb287
root <root@static.207.44.9.176.clients.your-server.de>
2016-04-05 06:43:38 +0200

    backup letsencrypt certs

--- dump.sh
+++ dump.sh
@@ -1,7 +1,1 @@
-rm -f dump.tar.bz2
-for x in tinkuy-nodebb tinkuy-redis
-do
-  echo dumping $x
-  tar rpPf dump.tar /var/lib/docker/volumes/whale_$x
-done
-bzip2 -9 dump.tar
+tar cvpP /var/lib/docker/volumes/whale_* | xz -9 > dump.tar.xz
---
+++ restart.sh
@@ -0,0 +1,2 @@
+docker-compose build $@
+docker-compose up --no-deps -d $@
