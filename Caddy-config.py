fc6f6d113e6372c9d52d914fb77936b42c3cf966
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-02 20:17:48 +0200

    domain per app

---
+++ Caddy-config.py
@@ -0,0 +1,14 @@
+for line in open('Caddy-orig'):
+    print line,
+for line in open('Caddy-repos'):
+    parts = line.strip().split('/')
+    user = parts[0]
+    repos = parts[1]
+
+    print repos + '.solsort.com {'
+    print '  git github.com/' + user + '/' + repos + ' ' + repos + ' {'
+    print '    hook /git-update'
+    print '  }'
+    print '  root ' + repos
+    print '  cors'
+    print '}'
