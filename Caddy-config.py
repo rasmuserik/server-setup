4458b553a0667a640421fed83906b37da2de7ef0
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-03 08:31:38 +0200

    update project-list to contain more info

--- Caddy-config.py
+++ Caddy-config.py
@@ -1,14 +1,22 @@
+def get(arr, pos, default):
+    try:
+        return arr[pos]
+    except IndexError:
+        return default
+
 for line in open('Caddy-orig'):
     print line,
-for line in open('Caddy-repos'):
-    parts = line.strip().split('/')
-    user = parts[0]
-    repos = parts[1]
-
-    print repos + '.solsort.com {'
-    print '  git github.com/' + user + '/' + repos + ' {'
-    print '    hook /git-update'
-    print '  }'
-    print '  root /apps/' + repos
-    print '  cors'
-    print '}'
+for line in open('projects.lst'):
+    parts = line.strip().split(' ')
+    kind = parts[1]
+    if kind == 'git':
+      user = get(parts, 3, 'solsort')
+      repos = parts[2]
+  
+      print repos + '.solsort.com {'
+      print '  git github.com/' + user + '/' + repos + ' {'
+      print '    hook /git-update'
+      print '  }'
+      print '  root /apps/' + repos
+      print '  cors'
+      print '}'
