e3d15ce5801294656926ee5a5b9bf04492b4c2f0
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-06-01 14:48:08 +0200

    update update.sh

--- update.sh
+++ update.sh
@@ -9,9 +9,4 @@ then
   ssh root@borg.solsort.com "source ~/.pw && cd whale && docker kill whale_${1}_1; docker-compose up -d"
 fi
 
-ssh root@borg.solsort.com "
-  cd apps &&
-  git pull &&
-  ./update.sh" &
-
 ssh root@borg.solsort.com "cd whale; docker-compose logs"
