014e8c3f0d82fc6cf10d5009f6b6c6dc7eb20e99
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-07-05 18:39:02 +0200

    progress on html generation

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject solsort.www "0.0.0-SNAPSHOT"
   :description ""
   :dependencies [[happy "0.5.2"]
+                 [org.clojure/clojure "1.8.0"]
                  [hiccup "1.0.5"]
-                 [garden "1.3.2"]]
+                 [garden "1.3.2"]
+                 ]
   :main solsort.www)
--- src/solsort/www.clj
+++ src/solsort/www.clj
@@ -1,13 +1,15 @@
-(ns solsort.www)
-
-(defn apps
+(ns solsort.www
+  (:use [hiccup.page :only (html5)])
+  (:require [clojure.string :as str])
+  )
+(def apps-list
   (rest
    [nil
     [:git 2016  7 "solsort/www"]
     [:git 2016  7 "solsort/fmtools"]
-    [:web 2016  5 "https://openplatform.dbc.dk"]
+    [:web 2016  5 "https://openplatform.dbc.dk" "Den Åbne Platform"]
     [:wordpress 2016  5 "alive.solsort.com"]
-    [:web 2016  4 "https://forum.tinkuy.dk"]
+    [:web 2016  4 "https://forum.tinkuy.dk" "Tinkuy Forum"]
     [:git 2016  1 "rasmuserik/mubackend"]
     [:git 2016  1 "rasmuserik/apps"]
     [:wordpress 2016  0 "rasmuserik.com"]
@@ -35,4 +37,56 @@
     [:git 2011  3 "rasmuserik/js1k-rain"]
     [:git 2011  3 "rasmuserik/js1k-brownian"]
     [:git 2011  0 "rasmuserik/timelog"]
-    [:git 2011  0 "rasmuserik/combigame"]]))
+    [:git 2011  0 "rasmuserik/combigame"]]
+   ))
+
+(def month-names
+  ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+(defn git [id]
+  (let [short-name (second (str/split id #"/"))
+        url (str "https://" short-name ".solsort.com")]
+    {:url url
+     :icon (str url "/icon.png")})
+  )
+(defn web [id title]
+  {:url id :title title})
+(defn wp [id]
+  {:url (str "https://" id)})
+
+(defn app-to-obj [[type year month & args]]
+  (print 'here)
+  (into {:type type
+          :year year
+         :month (get month-names month)
+         :args args}
+        (apply ({:git git
+                 :web web
+                 :wordpress wp} type) args)))
+
+(defn entry [o]
+  [:div.entry
+   [:img.icon {:src (:icon o)}]
+   [:div.date (:month o) (:year o)]
+   [:div.title (:title o)]])
+
+(->> apps-list
+     (map app-to-obj)
+     )
+
+(println (map app-to-obj apps-list))
+
+(defn htmldoc []
+  (html5 
+   [:head [:title "solsort.com"]]
+   [:body
+    [:h1 "hello"]
+    (into [:div]
+          (->> apps-list
+               (map app-to-obj)
+               (map entry)))
+    ]))
+(println (htmldoc))
+
+(defn -main [& args]
+  (println 'main (html5 [:h1 "hello"]) args))
+
