e47f860146940866df60c67e1523ae47528e1845
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-15 06:26:36 +0200

    also backup logs

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 install -d ~/data/backup
 cd ~/data/backup
-ssh root@borg.solsort.com "tar cvpP /var/lib/docker/volumes/whale_* | xz -9 " > whale-latest.tar.xz
+ssh root@borg.solsort.com "tar cvpP /var/lib/docker/volumes/whale_* /var/lib/docker/containers | xz -9 " > whale-latest.tar.xz
 rsync -avz root@bork.solsort.com:/var/lib/docker/containers whale-log
 ln -f whale-latest.tar.xz whale-`date +%A`.tar.xz
 ln -f whale-latest.tar.xz whale-`date +%Gw%V`.tar.xz
