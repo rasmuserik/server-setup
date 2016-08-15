0c19827f3737b1fc47fa67022cd572f68de36d51
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-15 17:58:43 +0200

    better backup

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
+#!/bin/bash -v
 install -d /home/rasmuserik/data/backup/borg
 rsync -avr root@borg.solsort.com:/var/lib/docker/containers/ /home/rasmuserik/data/backup/borg/containers/
 rsync -avr root@borg.solsort.com:whale/ /home/rasmuserik/data/backup/borg/whale/
 rsync -avr root@borg.solsort.com:/var/lib/docker/volumes/ /home/rasmuserik/data/backup/borg/volumes/
 mv /home/rasmuserik/data/backup/borg.prev.tar.xz /home/rasmuserik/data/backup/borg.prevprev.tar.xz 
 mv /home/rasmuserik/data/backup/borg.latest.tar.xz /home/rasmuserik/data/backup/borg.prev.tar.xz 
-tar cvp /home/rasmuserik/data/backup/borg | xz -9 > /home/rasmuserik/data/backup/borg.latest.tar.xz
+tar cp /home/rasmuserik/data/backup/borg/containers/*/*.log | xz -9 > /home/rasmuserik/data/backup/borg-logs.`date +%F`.tar.xz
+tar cp /home/rasmuserik/data/backup/borg | xz -9 > /home/rasmuserik/data/backup/borg.latest.tar.xz
 cp /home/rasmuserik/data/backup/borg.latest.tar.xz /home/rasmuserik/data/backup/borg-all.`date +%Y-%m`.tar.xz 
-tar cvp /home/rasmuserik/data/backup/borg/containers/*/*.log | xz -9 > /home/rasmuserik/data/backup/borg-logs.`date +%F`.tar.xz
-
