94936a4bb59a3e2cec766ecfb688919bfeab1d4e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-16 20:28:11 +0200

    use gzip for backup

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@ install -d /home/rasmuserik/data/backup/borg
 rsync -avr root@borg.solsort.com:/var/lib/docker/containers/ /home/rasmuserik/data/backup/borg/containers/
 rsync -avr root@borg.solsort.com:whale/ /home/rasmuserik/data/backup/borg/whale/
 rsync -avr root@borg.solsort.com:/var/lib/docker/volumes/ /home/rasmuserik/data/backup/borg/volumes/
-mv /home/rasmuserik/data/backup/borg.prev.tar.xz /home/rasmuserik/data/backup/borg.prevprev.tar.xz 
-mv /home/rasmuserik/data/backup/borg.latest.tar.xz /home/rasmuserik/data/backup/borg.prev.tar.xz 
-tar cp /home/rasmuserik/data/backup/borg/containers/*/*.log | xz -9 > /home/rasmuserik/data/backup/borg-logs.`date +%F`.tar.xz
-tar cp /home/rasmuserik/data/backup/borg | xz -9 > /home/rasmuserik/data/backup/borg.latest.tar.xz
-cp /home/rasmuserik/data/backup/borg.latest.tar.xz /home/rasmuserik/data/backup/borg-all.`date +%Y-%m`.tar.xz 
+mv /home/rasmuserik/data/backup/borg.prev.tar.gz /home/rasmuserik/data/backup/borg.prevprev.tar.gz 
+mv /home/rasmuserik/data/backup/borg.latest.tar.gz /home/rasmuserik/data/backup/borg.prev.tar.gz 
+tar cp /home/rasmuserik/data/backup/borg/containers/*/*.log | gz -9 > /home/rasmuserik/data/backup/borg-logs.`date +%F`.tar.gz
+tar cp /home/rasmuserik/data/backup/borg | gzip -9 > /home/rasmuserik/data/backup/borg.latest.tar.gz
+cp /home/rasmuserik/data/backup/borg.latest.tar.gz /home/rasmuserik/data/backup/borg.`date +%Y-%m`.tar.gz 
