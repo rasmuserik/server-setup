52d9af61c616f7f9b67e9306587f27843e8e4d41
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-08-16 20:40:04 +0200

    xz logs

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ rsync -avr root@borg.solsort.com:whale/ /home/rasmuserik/data/backup/borg/whale/
 rsync -avr root@borg.solsort.com:/var/lib/docker/volumes/ /home/rasmuserik/data/backup/borg/volumes/
 mv /home/rasmuserik/data/backup/borg.prev.tar.gz /home/rasmuserik/data/backup/borg.prevprev.tar.gz 
 mv /home/rasmuserik/data/backup/borg.latest.tar.gz /home/rasmuserik/data/backup/borg.prev.tar.gz 
-tar cp /home/rasmuserik/data/backup/borg/containers/*/*.log | gzip -9 > /home/rasmuserik/data/backup/borg-logs.`date +%F`.tar.gz
+tar cp /home/rasmuserik/data/backup/borg/containers/*/*.log | xz -9 > /home/rasmuserik/data/backup/borg-logs.`date +%F`.tar.xz
 tar cp /home/rasmuserik/data/backup/borg | gzip -9 > /home/rasmuserik/data/backup/borg.latest.tar.gz
 cp /home/rasmuserik/data/backup/borg.latest.tar.gz /home/rasmuserik/data/backup/borg.`date +%Y-%m`.tar.gz 
